64fead17eb70cf8a47ef32f0319500a3cfdb6f83
disor <disorn.bootso@gmail.com>
2021-10-23 10:30:21 +0700

    move dv and change path for build .jar file

--- src/main/java/th/ac/chula/cafetps/Constants.java
+++ src/main/java/th/ac/chula/cafetps/Constants.java
@@ -2,5 +2,5 @@ package th.ac.chula.cafetps;
 
 public class Constants {
     /* Changes to .gitignore */
-    static final String DATABASE_NAME = "jdbc:sqlite:src/main/resources/th/ac/chula/cafetps/Cafe.sqlite";
+    static final String DATABASE_NAME = "jdbc:sqlite:src/database/Cafe.sqlite";
 }
