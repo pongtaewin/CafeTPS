ae3ac9630f67a4738ece44415e46d7d8f1072bfb
Pongtaewin Nakpongpiman <32708251+pongtaewin@users.noreply.github.com>
2021-10-23 10:09:30 +0700

    Move Helper for MonthController to SummaryHelper

--- src/main/java/th/ac/chula/cafetps/Helper.java
+++ src/main/java/th/ac/chula/cafetps/Helper.java
@@ -8,10 +8,10 @@ import th.ac.chula.cafetps.model.Member;
 import th.ac.chula.cafetps.model.User;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.HashMap;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
 
 public class Helper {
 
@@ -95,8 +95,6 @@ public class Helper {
                 ResultSet resultSet = statement.executeQuery(getRID);
                 resultSet.next();
                 r_id = resultSet.getInt(1);
-                // TODO: remove debug println
-                // System.out.println(r_id);
                 PreparedStatement preparedStatement = connection.prepareStatement(command);
 
                 for (Item item : receipt) {
@@ -121,135 +119,5 @@ public class Helper {
                 .findFirst();
     }
 
-    //Summary Helper
-    public ArrayList<String> getDistinctYearMonth(){
-        Connection connection = DatabaseHelper.connect();
-        ArrayList<String> item = new ArrayList<>();
-        String sql = "select distinct(strftime('%Y-%m',create_date)) from Receipt";
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()){
-                item.add(resultSet.getString(1));
-            }
-            connection.close();
-        }catch (SQLException e){
-            e.printStackTrace();
-        }return item;
-    }
-
-    public HashMap<String,Integer> getSellUnit(String yearAndMonth){
-        Connection connection = DatabaseHelper.connect();
-        HashMap<String,Integer> item = new HashMap<>();
-        String sql = """
-                select temp.category,sum(temp.amount) as sale_unit from (select r_id,Item.category,amount from Receipt_Detail\s
-                inner join  Item
-                on Receipt_Detail.item_id = Item.item_id
-                order by r_id) as temp
-                where temp.r_id in (select r_id from Receipt where strftime('%Y-%m',create_date)  = ?)
-                group by category""";
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1,yearAndMonth);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()){
-                item.put(resultSet.getString("category"),resultSet.getInt("sale_unit"));
-            }
-            connection.close();
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-        return item;
-    }
-
-    public ArrayList<Integer> getIncomeAndCost(String yearAndMonth){
-        Connection connection = DatabaseHelper.connect();
-        ArrayList<Integer> item = new ArrayList<>();
-        String sql = """
-                select sum(income) as total_income,sum(cost) as total_cost,sum(income)-sum(cost) as sell_profit from (select r_id,Item.item_id,priceperunit*amount as income,costperunit*amount as cost from Item
-                inner join (select r_id,item_id as id,amount from Receipt_Detail
-                where r_id in (select r_id from Receipt where strftime('%Y-%m',create_date)  = ?))
-                on Item.item_id = id)""";
-        try {
-            PreparedStatement preparedStatement = DatabaseHelper.connect().prepareStatement(sql);
-            preparedStatement.setString(1,yearAndMonth);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            resultSet.next();
-            item.add(resultSet.getInt("total_income"));
-            item.add(resultSet.getInt("total_cost"));
-            item.add(resultSet.getInt("sell_profit"));
-            connection.close();
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-        return item;
-    }
-
-    public int getEmployeeTotalSalary(){
-        Connection connection = DatabaseHelper.connect();
-        int total = 0;
-        String sql = "select sum(salary) from Employee";
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sql);
-            resultSet.next();
-            total = resultSet.getInt(1);
-            connection.close();
-        }catch (SQLException e){
-            e.printStackTrace();
-        }return total;
-    }
-
-    public int getRentalCost(){
-        Connection connection = DatabaseHelper.connect();
-        int rental = 0;
-        String sql = "select * from Cost where subject = 'rental'";
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sql);
-            resultSet.next();
-            rental = resultSet.getInt("price");
-            connection.close();
-        }catch (SQLException e){
-            e.printStackTrace();
-        }return rental;
-    }
-
-    public HashMap<String,ArrayList<XYChart.Data<String,Integer>>> getChartData(String yearAndMonth){
-        HashMap<String,ArrayList<XYChart.Data<String,Integer>>> data = new HashMap<>();
-        ArrayList<XYChart.Data<String,Integer>> bakeryline = new ArrayList<>();
-        ArrayList<XYChart.Data<String,Integer>> coffeeline = new ArrayList<>();
-        ArrayList<XYChart.Data<String,Integer>> noncoffeeline = new ArrayList<>();
-        data.put("bakery",bakeryline);
-        data.put("coffee",coffeeline);
-        data.put("noncoffee",noncoffeeline);
-
-        Connection connection = DatabaseHelper.connect();
-        String sql = """
-                select strftime('%d',create_date) as day,sum(amount) as total,category from Receipt_Detail
-                inner join Item
-                on Receipt_Detail.item_id = Item.item_id
-                inner join (select * from Receipt where strftime('%Y-%m',create_date)  = ?) as temp
-                on Receipt_Detail.r_id = temp.r_id
-                group by category,day;""";
-        try{
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1,yearAndMonth);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()){
-                if(resultSet.getString("category").equals("bakery")){
-                    bakeryline.add(new XYChart.Data<>(resultSet.getInt("day")+"",resultSet.getInt("total")));
-                }else if (resultSet.getString("category").equals("coffee")){
-                    coffeeline.add(new XYChart.Data<>(resultSet.getInt("day")+"",resultSet.getInt("total")));
-                }else if (resultSet.getString("category").equals("noncoffee")){
-                    noncoffeeline.add(new XYChart.Data<>(resultSet.getInt("day")+"",resultSet.getInt("total")));
-                }
-            }
-            connection.close();
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-        return data;
-    }
 }
 
